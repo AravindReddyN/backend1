6f96c6b1b8e1d004e5f63ca53038e03a37cfbf2c
AravindReddyN <150883186+AravindReddyN@users.noreply.github.com>
2023-12-20 11:14:41 +0530

    Update mono.js

--- mono.js
+++ mono.js
@@ -4,7 +4,7 @@ const express=require("express")
 const cors=require("cors")
 // const cookieParser=require('cookie-parser')
 const app=express();
-app..use(cors({
+app.use(cors({
     origin: 'https://frontend-3wwe.onrender.com',
     credentials:true,
 }));
